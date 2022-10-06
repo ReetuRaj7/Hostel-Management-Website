446213c938c55556f7fe5093e32c08f285cc55b0
Reetu Raj Dubey <mreeturaj1234@gmail.com>
2022-10-06 14:52:17 +0530

    add sql schema and .env

--- user-schema.sql
+++ user-schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `hostel_manage`.`user` (
+CREATE TABLE `hostel`.`user` (
     `id` INT NOT NULL AUTO_INCREMENT ,
     `first_name` VARCHAR(45) NOT NULL ,
     `last_name` VARCHAR(45) NOT NULL ,
@@ -8,3 +8,17 @@ CREATE TABLE `hostel_manage`.`user` (
     `status` VARCHAR(10) NOT NULL DEFAULT 'active' ,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
+
+
+CREATE TABLE `hostel`.`resident` ( 
+    `res_id` INT NOT NULL , 
+    `res_first_name` VARCHAR(20) NOT NULL , 
+    `res_city` VARCHAR(20) NOT NULL , 
+    `res_state` VARCHAR(20) NOT NULL 
+) ENGINE = InnoDB;
+CREATE TABLE `hostel`.`accounts` ( 
+    `username` VARCHAR(20) NOT NULL , 
+    `password` VARCHAR(20) NOT NULL , 
+    PRIMARY KEY (`username`)
+) ENGINE = InnoDB;
+INSERT INTO `accounts`(`username`, `password`) VALUES ('user1234','trial123')
