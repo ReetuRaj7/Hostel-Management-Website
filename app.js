fb591c3aac2b64d116ac4e428f3edc14b971a3e5
ReetuRaj7 <mreeturaj1234@gmail.com>
2022-11-01 18:49:55 +0530

    logout button

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+var isLogged = false;
 
 app.use(express.urlencoded({extended: true}));
 
@@ -29,11 +30,21 @@ app.engine('hbs', exphbs({extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
 app.get('/',function(req,res){
+  if(!isLogged)
     res.sendFile(__dirname+'/signin.html');
+  else
+    res.sendFile(__dirname+'/front-page.html');
 });
 
 
 const routes = require('./server/routes/user');
+
+
+// app.post('/logout',(req,res)=>isLogged=false);
+app.get('/logout',function(req,res){
+  isLogged=false;
+  res.redirect('/')
+});
 
 app.post('/',function(req,res){
     var username = req.body.uname;
@@ -42,9 +53,10 @@ app.post('/',function(req,res){
 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results) {
 			if (error) throw error;
 			if (results.length > 0) {
+        isLogged = true;
 				res.sendFile(__dirname + '/front-page.html');
 			} else {
-                res.sendFile(__dirname + '/failure.html');
+        res.sendFile(__dirname + '/failure.html');
 			}
 			// res.end();
 		});
@@ -59,4 +71,6 @@ app.post('/fail',function(req,res){
     res.redirect('/');
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+var T = new Date();
+const time = T.toLocaleTimeString();
+app.listen(port, () => console.log(`Listening on port ${port}, since ${time}`));
